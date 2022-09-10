d4d39e2004a0f5fb67f18d10b58699e05184a2d3
Chairil ashar <asharchairil4@gmail.com>
2022-09-11 01:43:05 +0700

    initialproject

--- routes/barang.js
+++ routes/barang.js
@@ -1,10 +1,47 @@
 var express = require('express');
 var router = express.Router();
 
-module.exports = function(pool) {
-router.get('/', function(req, res, next) {
-  res.render('index');
-});
 
-return router;
+module.exports = function (pool) {
+    router.get('/', async function (req, res, next) {
+        try {
+            const { rows } = await pool.query('SELECT * FROM barang');
+            console.log(rows)
+            res.status(200).json(rows)
+        } catch (e) {
+            console.log(e)
+            res.status(500).json({message : 'ini eror'})
+        }
+
+    });
+
+    router.post('/', async function (req, res) {
+        try {
+            let sql = `INSERT INTO barang (id_barang, nama_barang) VALUES ($1, $2)`
+            const post = await pool.query(sql, [req.body.id_barang, req.body.nama_barang])
+            res.status(200).json(post)
+        } catch (e) {
+            console.log(e)
+            res.status(500).json({message : 'ini eror'})
+        }
+
+    });
+
+    router.put('/:id', async function (req, res) {
+        try {
+            let id = req.params.id_barang
+            let sql = 'SELECT * FROM barang WHERE id_barang = $1'
+            const editGet = await pool.query(sql, [id])
+            res.status(200).json(editGet)
+        } catch (e) {
+            console.log(e)
+            res.status(500).json({message : 'ini eror'})
+        }
+
+    });
+ 
+
+
+       
+    return router;
 }

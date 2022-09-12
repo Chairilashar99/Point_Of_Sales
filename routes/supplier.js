3953d6cb074d7202e0c418bfd672a339c432ba10
Chairil ashar <asharchairil4@gmail.com>
2022-09-12 11:50:19 +0700

    progres

--- routes/supplier.js
+++ routes/supplier.js
@@ -4,7 +4,7 @@ var router = express.Router();
 module.exports = function (pool) {
     router.get('/', async function (req, res, next) {
         try {
-            const { rows } = await pool.query('SELECT * FROM gudang');
+            const { rows } = await pool.query('SELECT * FROM supplier');
             console.log(rows)
             res.status(200).json(rows)
         } catch (e) {
@@ -16,8 +16,8 @@ module.exports = function (pool) {
 
     router.post('/', async function (req, res) {
         try {
-            let sql = `INSERT INTO gudang(id_gudang, nama_gudang, alamat_gudang) VALUES ($1, $2, $3)`
-            const post = await pool.query(sql, [req.body.id_gudang, req.body.nama_gudang, req.body.alamat_gudang])
+            let sql = `INSERT INTO supplier(id_supplier, nama_supplier, alamat_supplier, telepon, email) VALUES ($1, $2, $3, $4, $5)`
+            const post = await pool.query(sql, [req.body.id_supplier, req.body.nama_supplier, req.body.alamat_supplier, req.body.telepon, req.body.email])
             res.status(200).json(post)
         } catch (e) {
             console.log(e)
@@ -28,8 +28,8 @@ module.exports = function (pool) {
 
     router.get('/:id', async function (req, res) {
         try {
-            let id = req.params.id_gudang
-            let sql = 'SELECT * FROM gudang WHERE id_gudang = $1'
+            let id = req.params.id_supplier
+            let sql = 'SELECT * FROM supplier WHERE id_supplier = $1'
             const editGet = await pool.query(sql, [id])
             res.status(200).json(editGet)
         } catch (e) {
@@ -39,29 +39,33 @@ module.exports = function (pool) {
 
     });
 
-    router.put('/:id_gudang', async function (req, res) {
+    router.put('/:id_supplier', async function (req, res) {
         try {
-            let sql = `UPDATE gudang SET 
-          nama_gudang = $1,
-          alamat_gudang = $2
-          WHERE id_gudang = $3`
+            let sql = `UPDATE supplier SET 
+          nama_supplier = $1,
+          alamat_supplier = $2,
+          telepon = $3,
+          email = $4
+          WHERE id_supplier = $5`
 
             const edit = await pool.query(sql,
-                [req.body.nama_gudang,
-                req.body.alamat_gudang,
-                req.params.id_gudang]);
+                [req.body.nama_supplier,
+                req.body.alamat_supplier,
+                req.body.telepon,
+                req.body.email,
+                req.params.id_supplier]);
 
             res.status(200).json(edit)
         } catch (error) {
             console.log(error)
-            res.status(500).json({ message: "error edit gudang" })
+            res.status(500).json({ message: "error edit supplier" })
         }
     });
 
-    router.delete('/:id_gudang', async function (req, res) {
+    router.delete('/:id_supplier', async function (req, res) {
         try {
-            let id = req.params.id_gudang
-            let sql = `DELETE FROM gudang WHERE id_gudang= $1`;
+            let id = req.params.id_supplier
+            let sql = `DELETE FROM supplier WHERE id_supplier= $1`;
 
             const hapus = await pool.query(sql, [id])
             res.status(200).json(hapus)

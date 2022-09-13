15180413904619be688072e2f0a0f9fa56199398
Chairil ashar <asharchairil4@gmail.com>
2022-09-13 19:50:43 +0700

    progress

--- routes/barang.js
+++ routes/barang.js
@@ -3,72 +3,101 @@ var router = express.Router();
 
 
 module.exports = function (pool) {
-    router.get('/', async function (req, res, next) {
-        try {
-            const { rows } = await pool.query('SELECT * FROM barang');
-            console.log(rows)
-            res.status(200).json(rows)
-        } catch (e) {
-            console.log(e)
-            res.status(500).json({message : 'ini eror'})
-        }
+  router.get('/', async function (req, res, next) {
+    const { json } = req.headers
 
-    });
+    try {
+      const { rows } = await pool.query('SELECT * FROM barang');
+      if (json == 'true') {
+        res.status(200).json(rows)
+      } else {
+        res.render('barang')
+      }
+    } catch (e) {
+      console.log(e)
+      res.status(500).json({ message: 'ini eror' })
+    }
 
-    router.post('/', async function (req, res) {
-        try {
-            let sql = `INSERT INTO barang (id_barang, nama_barang) VALUES ($1, $2)`
-            const post = await pool.query(sql, [req.body.id_barang, req.body.nama_barang])
-            res.status(200).json(post)
-        } catch (e) {
-            console.log(e)
-            res.status(500).json({message : 'ini eror'})
-        }
+  });
 
-    });
+  router.post('/', async function (req, res) {
+    const { json } = req.headers
 
-    router.get('/:id', async function (req, res) {
-        try {
-            let id = req.params.id_barang
-            let sql = 'SELECT * FROM barang WHERE id_barang = $1'
-            const editGet = await pool.query(sql, [id])
-            res.status(200).json(editGet)
-        } catch (e) {
-            console.log(e)
-            res.status(500).json({message : 'ini eror'})
-        }
+    try {
+      let sql = `INSERT INTO barang (id_barang, nama_barang) VALUES ($1, $2)`
+      const post = await pool.query(sql, [req.body.id_barang, req.body.nama_barang])
+      if (json == 'true') {
+        res.status(200).json(post)
+      } else {
+        res.render('barang')
+      }
+      
+    } catch (e) {
+      console.log(e)
+      res.status(500).json({ message: 'ini eror' })
+    }
 
-    });  
+  });
 
-    router.put('/:id_barang', async function (req, res) {
-        try {
-          let sql = `UPDATE barang SET 
+  router.get('/:id', async function (req, res) {
+    const { json } = req.headers
+
+    try {
+      let id = req.params.id_barang
+      let sql = 'SELECT * FROM barang WHERE id_barang = $1'
+      const editGet = await pool.query(sql, [id])
+      if (json == 'true') {
+        res.status(200).json(editGet)
+      } else {
+        res.render('barang')
+      }
+    } catch (e) {
+      console.log(e)
+      res.status(500).json({ message: 'ini eror' })
+    }
+
+  });
+
+  router.put('/:id_barang', async function (req, res) {
+    const { json } = req.headers
+
+    try {
+      let sql = `UPDATE barang SET 
           nama_barang = $1
           WHERE id_barang = $2`
-    
-          const edit = await pool.query(sql,
-            [req.body.nama_barang,
-            req.params.id_barang]);
-    
+
+      const edit = await pool.query(sql,
+        [req.body.nama_barang,
+        req.params.id_barang]);
+
+        if (json == 'true') {
           res.status(200).json(edit)
-        } catch (error) {
-          console.log(error)
-          res.status(500).json({ message: "error edit barang" })
+        } else {
+          res.render('barang')
         }
-      });
-    
-      router.delete('/:id_barang', async function (req, res) {
-        try {
-          let id = req.params.id_barang
-          let sql = `DELETE FROM barang WHERE id_barang= $1`;
-    
-          const hapus = await pool.query(sql, [id])
-          res.status(200).json(hapus)
-        } catch (error) {
-          res.status(500)
-        }
-    
-      });
-    
-    return router;
+    } catch (error) {
+      console.log(error)
+      res.status(500).json({ message: "error edit barang" })
+    }
+  });
+
+  router.delete('/:id_barang', async function (req, res) {
+    const { json } = req.headers
+    try {
+      let id = req.params.id_barang
+      let sql = `DELETE FROM barang WHERE id_barang= $1`;
+
+      const hapus = await pool.query(sql, [id])
+      if (json == 'true') {
+        res.status(200).json(hapus)
+      } else {
+        res.render('barang')
+      }
+    } catch (error) {
+      res.status(500)
+    }
+
+  });
+
+  return router;
 }

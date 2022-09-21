aa0f23f74b3061ac0f0768d1824cb1c60685d671
Chairil ashar <asharchairil4@gmail.com>
2022-09-21 17:11:11 +0700

    progresss

--- routes/varian.js
+++ routes/varian.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var pool = require('pg');
 var path = require('path')
 var moment = require('moment');
+const fileUpload = require('express-fileupload');
 
 
 module.exports = function (pool) {
@@ -59,78 +60,36 @@ module.exports = function (pool) {
     })
 
 
-    router.get('/api/:id', async function (req, res) {
-        const { json } = req.headers
 
-        try {
-            let id = req.params.id
-            let sql = `SELECT var.barcode,
-        var.varian_name,
-        bar.id_barang,
-        bar.nama_barang,
-          var.stock,
-          var.buy_price,
-          var.sell_price,
-          var.pictures
-FROM varian var
-INNER JOIN barang bar ON bar.id_barang = var.id_barang WHERE var.id_barang = $1`
-            const { rows } = await pool.query(sql, [id])
-            if (json == 'true') {
-                res.status(200).json(rows)
-            } else {
-                res.render('varian')
-            }
-        } catch (e) {
-            console.log(e)
-            res.status(500).json({ message: 'ini eror' })
-        }
+//     router.get('/info/:id', async function (req, res) {
+//         const { json } = req.headers
 
-    });
+//         try {
+//             let id = req.params.id
+//             let sql = `SELECT var.barcode,
+//         var.varian_name,
+//         bar.id_barang,
+//         bar.nama_barang,
+//           var.stock,
+//           var.buy_price,
+//           var.sell_price,
+//           var.pictures
+// FROM varian var
+// INNER JOIN barang bar ON bar.id_barang = var.id_barang WHERE barcode = $1;`
+//             const { rows } = await pool.query(sql, [id])
+//             if (json == 'true') {
+//                 res.status(200).json(rows)
+//             } else {
+//                 res.render('varian')
+//             }
+//         } catch (e) {
+//             console.log(e)
+//             res.status(500).json({ message: 'ini eror' })
+//         }
 
-    router.get('/info/:id', async function (req, res) {
-        const { json } = req.headers
+//     });
 
-        try {
-            let id = req.params.id
-            let sql = `SELECT var.barcode,
-        var.varian_name,
-        bar.id_barang,
-        bar.nama_barang,
-          var.stock,
-          var.buy_price,
-          var.sell_price,
-          var.pictures
-FROM varian var
-INNER JOIN barang bar ON bar.id_barang = var.id_barang WHERE barcode = $1;`
-            const { rows } = await pool.query(sql, [id])
-            if (json == 'true') {
-                res.status(200).json(rows)
-            } else {
-                res.render('varian')
-            }
-        } catch (e) {
-            console.log(e)
-            res.status(500).json({ message: 'ini eror' })
-        }
 
-    });
-
-    router.get('/add', async function (req, res) {
-        const { json } = req.headers
-
-        try {
-            const { rows } = await pool.query('SELECT * FROM barang');
-            if (json == 'true') {
-                res.status(200).json(rows)
-            } else {
-                res.render('varian_add')
-            }
-        } catch (e) {
-            console.log(e)
-            res.status(500).json({ message: 'ini eror' })
-        }
-
-    });
 
     router.post('/', async function (req, res) {
         // console.log(req.body, req.files)
@@ -180,7 +139,7 @@ INNER JOIN barang bar ON bar.id_barang = var.id_barang WHERE barcode = $1;`
         }
     })
 
-    router.get('/:barcode',  async function (req, res) {
+    router.get('/:id', async function (req, res) {
         const { json } = req.headers
 
         try {
@@ -208,59 +167,75 @@ INNER JOIN barang bar ON bar.id_barang = var.id_barang WHERE barcode = $1;`
 
     });
 
-
-    router.post('/edit/:id', async function (req, res) {
+    router.put('/:id', async function (req, res) {
+        console.log('body', req.body)
         const { json } = req.headers
+        try {
+            const { barcodeedit, varian_nameedit, stockedit, buy_priceedit, sell_priceedit, id_barangedit } = req.body
 
-        try{
-        let pictures;
-        let uploadPath;
-        if (!req.files || Object.keys(req.files).length === 0) {
-           
-            // The name of the input field (i.e. "pictures") is used to retrieve the uploaded file
-            pictures = req.files.gambar;
-            const filename = `A${Date.now()}-${pictures.name}`
-            uploadPath = path.join(__dirname, '/../public', 'pictures', filename);
-            // Use the mv() method to place the file somewhere on your server
-            pictures.mv(uploadPath, function (err) {
-                if (err)
-                    return res.status(500).send(err);
-                //  const {custom_input, nama, barang, stok, harga, satuan, gudang } = req.body
+            let pictures;
+            let uploadPath;
+            if (!req.files || Object.keys(req.files).length === 0) {
+                const edit = pool.query(`UPDATE varian set 
+                varian_name = $1,
+                stock = $2,
+                  sell_price = $3,
+                   buy_price = $4,
+                  id_barang = $5
+              WHERE barcode = $6`, [varian_nameedit, stockedit, buy_priceedit, sell_priceedit, id_barangedit, barcodeedit])
+                if (json == 'true') {
+                    res.status(200).json(edit)
+                } else {
+                    res.render('varian')
+                }
+            } else {
+                // The name of the input field (i.e. "pictures") is used to retrieve the uploaded file
+                pictures = req.files.gambaredit;
+                const filename = `A${Date.now()}-${pictures.name}`
+                uploadPath = path.join(__dirname, '/../public', 'images', filename);
+                // Use the mv() method to place the file somewhere on your server
+                pictures.mv(uploadPath, function (err) {
 
-                db.query(`UPDATE varian set 
+                    const { barcodeedit, varian_nameedit, stockedit, buy_priceedit, sell_priceedit, id_barangedit } = req.body
+                    const filename1 = `{${filename}}`
+                    console.log('fileUpload', req.body)
+                    const editfile = pool.query(`UPDATE varian set 
                 varian_name = $1,
                 stock = $2,
                  pictures = $3,
                   sell_price = $4,
                    buy_price = $5,
-                  id_barang = $6,
-              WHERE barcode = $7`, [nama, stok, saved_pictures, harga_jual, harga_beli, barang, custom_input], (err) => {
-                    if (err) {
-                        return console.error(err.message);
+                  id_barang = $6
+              WHERE barcode = $7`, [varian_nameedit, stockedit, filename1, buy_priceedit, sell_priceedit, id_barangedit, barcodeedit])
+                    if (json == 'true') {
+                        res.status(200).json(editfile)
+                    } else {
+                        res.render('varian')
                     }
-                    res.redirect('varian')
                 })
-            })
+            }
+        } catch (eror) {
+            res.status(500).json({ message: 'upload gagal' })
         }
-    })
+    });
 
-router.delete('/:barcode', async function (req, res) {
-    const { json } = req.headers
-    try {
-        let id = req.params.barcode
-        let sql = `DELETE FROM varian WHERE barcode= $1`;
+    router.delete('/:barcode', async function (req, res) {
+        const { json } = req.headers
+        try {
+            let id = req.params.barcode
+            let sql = `DELETE FROM varian WHERE barcode= $1`;
 
-        const hapus = await pool.query(sql, [id])
-        if (json == 'true') {
-            res.status(200).json(hapus)
-          } else {
-            res.render('varian')
-          }
+            const hapus = await pool.query(sql, [id])
+            if (json == 'true') {
+                res.status(200).json(hapus)
+            } else {
+                res.render('varian')
+            }
         } catch (error) {
-        res.status(500)
-    }
+            res.status(500)
+        }
 
-});
+    });
 
-return router;
+    return router;
 }
